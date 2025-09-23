fa2337afabe962bd4643d6bd18d00b5884e7b862
alexjgriffith <griffitaj@gmail.com>
2025-09-23 15:25:37 -0400

    initial commit

---
+++ build-love-appimage.sh
@@ -0,0 +1,25 @@
+sudo apt-get install --assume-yes build-essential git make cmake autoconf automake \
+     libtool pkg-config libasound2-dev libpulse-dev libaudio-dev \
+     libjack-dev libx11-dev libxext-dev libxrandr-dev libxcursor-dev \
+     libxfixes-dev libxi-dev libxinerama-dev libxxf86vm-dev libxss-dev \
+     libgl1-mesa-dev libdbus-1-dev libudev-dev libgles2-mesa-dev \
+     libegl1-mesa-dev libibus-1.0-dev fcitx-libs-dev libsamplerate0-dev \
+     libsndio-dev libwayland-dev libxkbcommon-dev libdrm-dev libgbm-dev
+
+git clone https://github.com/love2d/love.git
+cd love
+git switch 11.x
+## Patch CMake FIle to use Lua5.4 instead of Lua5.1
+sed -i 's/Lua51/Lua/g'
+mkdir -p build
+cd build
+cmake .. -DLOVE_JIT=0 -DCMAKE_INSTALL_PREFIX=/usr && make -j$(nproc)
+
+wget https://github.com/linuxdeploy/linuxdeploy/releases/download/1-alpha-20250213-2/linuxdeploy-x86_64.AppImage
+chmod +x linuxdeploy-x86_64.AppImage
+
+cat ../platform/unix/love.desktop.in | sed "s/@bindir@\/love %f/love \./g"> love.desktop
+
+cp ../platform/unix/love.svg .
+./linuxdeploy-x86_64.AppImage --appdir AppDir -e love -i love.svg -d love.desktop  --output appimage
+mv LÖVE-x86_64.AppImage love-11.5.AppImage
